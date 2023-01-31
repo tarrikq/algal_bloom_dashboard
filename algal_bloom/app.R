50c8c2a42df2c0a1a9cfcd1dc633866a1975f4d7
tarrikq <112569772+tarrikq@users.noreply.github.com>
2023-01-31 15:45:47 -0500

    Updated report
    Change reported percentages to auto update when model is run

--- algal_bloom/app.R
+++ algal_bloom/app.R
@@ -16,11 +16,12 @@ library(tensorflow)
 library(reticulate)	
 library(caret)
 library(shiny)
+library(kableExtra)
 py_path = "C:\\Users\\Tarri\\anaconda3\\" 
 use_python(py_path, required = T)	
 
-input <- read_csv("C:\\Users\\Tarri\\Desktop\\portfolio_projects\\algal_bloom_dashboard\\algal_bloom\\data\\input.csv")
-output <- read_csv("C:\\Users\\Tarri\\Desktop\\portfolio_projects\\algal_bloom_dashboard\\algal_bloom\\data\\output.csv")
+input_df <- read_csv("C:\\Users\\Tarri\\Desktop\\portfolio_projects\\algal_bloom_dashboard\\algal_bloom\\data\\input.csv")
+output_df <- read_csv("C:\\Users\\Tarri\\Desktop\\portfolio_projects\\algal_bloom_dashboard\\algal_bloom\\data\\output.csv")
 model = load_model_hdf5('C:/Users/Tarri/Desktop/portfolio_projects/algal_bloom_dashboard/code/nn_model.hdf5')
 
 
@@ -61,30 +62,38 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    
+    mydata <- reactive({
+      file <- input$file
+      # ext <- tools::file_ext(file$datapath)
+      # 
+      # req(file)
+      # validate(need(ext == "csv", "Please upload a csv file"))
+      
+      df <- read.csv(file$datapath)#, header = input$header)
+      
+      return(df)
+    })
+    
 
+    
+    # normalize <- function(x) {
+    #   return((x - min(x)) / (max(x) - min(x)))
+    # }
+    # 
+    # df <- as.data.frame(sapply(df, normalize))
+    output$contents <- renderTable({
+      kable(mydata(), "html", caption="Input Data") %>%
+        kable_styling() %>%
+        scroll_box(width = "100%", height = "500px")
+    })
   
-  output$contents <- renderTable({
-    
-    file <- input$file1
-    ext <- tools::file_ext(file$datapath)
-    
-    req(file)
-    validate(need(ext == "csv", "Please upload a csv file"))
-    
-    df <- read.csv(file$datapath, header = input$header)
-    
-    normalize <- function(x) {
-      return((x - min(x)) / (max(x) - min(x)))
-    }
-    
-    df <- as.data.frame(sapply(df, normalize))
-    print(df)
     
   ## Function to normalize all the columns in the dataset
 
   
   
-})}
+}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
